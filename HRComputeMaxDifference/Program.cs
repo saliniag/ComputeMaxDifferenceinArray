77ec7be86386ddd206886911a7012aad2f5c9311
saliniag <salini.agarwal11@gmail.com>
2017-09-01 14:45:53 -0700

    compute the largest difference in array

---
+++ HRComputeMaxDifference/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRComputeMaxDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+
+    class Difference {
+        //variables
+        private int[] elements;
+
+        public int maximumDifference;
+        //constructor
+        public Difference(int[] a)
+        {
+        this.elements= a;
+        }
+        public void computeDifference()
+        {
+            int min = elements[0];
+            int max = elements[0];
+            for (int i = 1; i < elements.Length; i++)
+            {
+                min = Math.Min(min, elements[i]);
+                max = Math.Max(max, elements[i]);
+            }
+            this.maximumDifference = Math.Abs(max - min);
+        }
+    }
+}
